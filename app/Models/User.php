5c3658ca609a132be9e4009b08ded47032e8891b
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2024-02-28 06:09:46 +0700

    add: adding tokens table

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function todos():HasMany
     {
         return $this->hasMany(Todo::class, 'user_id', 'id');
+    }
+
+    public function token():HasOne
+    {
+        return $this->hasOne(Token::class, 'user_id', 'id');
     }
 }
